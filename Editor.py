6189895e932d9a42cd3c5ccc3aab2ef991f56aee
Anwesh Mohanty <anwesh063@gmail.com>
2015-12-17 16:48:03 +0530

    plans to support tabs

--- Editor.py
+++ Editor.py
@@ -9,13 +9,13 @@ Now python2 support yay
 """
 
 import Tkinter as GUI
+import ttk
 import ScrolledText
 import commands as EC
 import os
 
 app = GUI.Tk()
-app2 = GUI.Tk()
-app2.withdraw()
+book = ttk.Notebook(app)
 lang = ''
 
 """
@@ -86,15 +86,15 @@ def main():
         linepad.yview('scroll', -1 * (event.delta / 120), 'units')
         return 'break'
 
-    frame1 = GUI.Frame(app)
-    frame12 = GUI.Frame(app)
-    frame2 = GUI.Frame(app)
+    frameA = GUI.Frame(book)
+    frame1 = GUI.Frame(frameA)
+    frame12 = GUI.Frame(frameA)
+    frame2 = GUI.Frame(frameA)
     W1 = GUI.PanedWindow(frame1, height=30, width=70)
     bar_editor = GUI.Scrollbar(frame1)
-    bar_input_h = GUI.Scrollbar(frame12, orient=GUI.HORIZONTAL)
 
     # 272822
-    lb = GUI.Listbox(app2, height=10, width=30)
+
     pad = GUI.Text(
         W1,
         height=30,
@@ -115,17 +115,20 @@ def main():
         insertbackground='white',
         state=GUI.DISABLED)
 
+    lb = GUI.Listbox(frame12, height=4, width=120)
+    lb.config(fg='gray',bg='#002b36')
+    # lb.pack(fill=GUI.X)
+
     inputpad = GUI.Text(
         W1,
         height=30,
-        width=30,
-        xscrollcommand=bar_input_h.set)
+        width=30)
     inputpad.config(fg='white', bg='#002b36', insertbackground='white')
+
     W1.add(pad)
     W1.add(inputpad)
 
     bar_editor.config(command=shareBar)
-    bar_input_h.config(command=inputpad.xview)
     # 2aa198
     outputpad = ScrolledText.ScrolledText(frame2, height=5, width=80)
     outputpad.config(
@@ -160,9 +163,9 @@ def main():
 
     runmenu = GUI.Menu(menubar, tearoff=0)
     runmenu.add_command(label='Compile - (F7)', command=lambda: run.compile(
-        app, pad, outputpad, lang))
+        app, pad, outputpad, lang, frame2))
     runmenu.add_command(label='Run - (F5)', command=lambda: run.run(
-        app, pad, outputpad, inputpad, lang))
+        app, pad, outputpad, inputpad, lang, frame2))
     menubar.add_cascade(label='Run', menu=runmenu)
 
     langmenu = GUI.Menu(menubar, tearoff=0)
@@ -171,31 +174,36 @@ def main():
     menubar.add_cascade(label='Language', menu=langmenu)
 
     pad.bind('<Tab>', lambda event: Display.tab_width(pad, event))
-    app2.bind('<Escape>', lambda event: Display.escape(app2, event))
-    app2.bind('<Tab>', lambda event: Display.select_first(lb, event))
-    app2.bind('<Return>', lambda event: Display.insert_word(app2, pad, lb, lang, event))
-    app.bind('<KeyPress>', lambda event: Display.show_in_console(
-        event, app2, pad, linepad, lang, lb))
-    app.bind('<space>', lambda event: Display.add_to_trie(app2, pad, lb, event))
-    app.bind('<Return>', lambda event: Display.indentation(pad, linepad, lang, event))
-    app.bind('<F7>', lambda event: run.compile(app, pad, outputpad, lang, event))
-    app.bind('<F5>', lambda event: run.run(
-        app, pad, outputpad, inputpad, lang, event))
+    app.bind('<Escape>', lambda event: Display.escape(frame12,pad, event))
+    app.bind('<Down>', lambda event: Display.select_first(frame12,lb ,pad,event))
+    frame12.bind('<Tab>', lambda event: Display.insert_word(frame12, pad, lb, lang, event))
 
-    app.bind('<Control-r>', lambda event: edit.redo(pad, linepad, event))
-    app.bind('<Control-z>', lambda event: edit.undo(pad, linepad, event))
+    app.bind('<KeyPress>', lambda event: Display.show_in_console(
+        event, pad, linepad, lang, lb,frame1,W1,bar_editor,frame2,frame12,outputpad))
+
+    app.bind('<space>', lambda event: Display.add_to_trie(frame12, pad, lb, event))
+    app.bind('<Return>', lambda event: Display.indentation(pad, linepad, lang, event))
+    app.bind('<F7>', lambda event: run.compile(app, pad, outputpad, lang, frame2, event))
+    app.bind('<F5>', lambda event: run.run(
+        app, pad, outputpad, inputpad, lang,frame2, event))
+
+    app.bind('<Control-r>', lambda event: edit.redo(pad, linepad, lang, event))
+    app.bind('<Control-z>', lambda event: edit.undo(pad, linepad, lang, event))
     app.bind('<BackSpace>', lambda event: Display.fast_backspace(pad, linepad, event))
+
+    book.add(frameA,text='Tab 1')
+
+    book.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
+    frameA.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
 
     frame1.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
     linepad.pack(side=GUI.LEFT, fill=GUI.Y)
     W1.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
-    # pad.pack(side = LEFT,fill=BOTH, expand=YES)
     bar_editor.pack(side=GUI.LEFT, fill=GUI.Y)
-    # inputpad.pack(side = LEFT,fill=Y)
-    frame12.pack(side=GUI.TOP, fill=GUI.BOTH)
-    frame2.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
-    bar_input_h.pack(side=GUI.RIGHT)
-    outputpad.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+    frame12.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+    lb.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+    # frame2.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+    # outputpad.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
     app.config(menu=menubar)
 
 
@@ -203,4 +211,3 @@ if __name__ == '__main__':
     main()
     app.title('untitled.cpp')
     app.mainloop()
-    app2.mainloop()
