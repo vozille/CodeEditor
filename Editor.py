22a402e990d6b614d9736cdbe6239a5200fe553b
Anwesh Mohanty <anwesh063@gmail.com>
2015-12-20 22:35:24 +0530

    added tabs

--- Editor.py
+++ Editor.py
@@ -11,37 +11,115 @@ Now python2 support yay
 import Tkinter as GUI
 import ttk
 import ScrolledText
-import commands as EC
-import os
+import commands
 
-app = GUI.Tk()
-book = ttk.Notebook(app)
 lang = ''
 
-"""
-Variables used here :
-:param app: the main window
-:param app2: the window for autocomplete
-:param pad: the textbox in which code is written
-:param lb: the listbox storing autocomplete words
-:param lang: programming language
-:param linepad: textbox contain lines
-:param inputpad: textbox for storing input
-:param outputpad: textbox for storing output
-:param args: mostly to pass event bindings
+class DisplayWidget():
+    def __init__(self, book):
+        """
+        :param book: the tabbed notebook
+        :return:
+        """
+        self.book = book
+        self.primary_frame = []
+        self.frame_codepad = []
+        self.frame_outputpad = []
+        self.frame_autocomplete = []
+        self.bar_editor = []
+        self.lb = []
+        self.pad = []
+        self.linepad = []
+        self.inputpad = []
+        self.outputpad = []
+        self.W1 = []
 
-:return:
-"""
+    def create_tab(self, *args):
+
+        primary_frame = GUI.Frame(self.book)
+        frame_codepad = GUI.Frame(primary_frame)
+        frame_autocomplete = GUI.Frame(primary_frame)
+        frame_outputpad = GUI.Frame(primary_frame)
+        W1 = GUI.PanedWindow(frame_codepad, height=30, width=70)
+        bar_editor = GUI.Scrollbar(frame_codepad)
+        pad = GUI.Text(W1, height=30, width=60, yscrollcommand=bar_editor.set, undo=True)
+        pad.config(fg='#f8f8f2', bg='#002b36', insertbackground='white')
+        linepad = GUI.Text(frame_codepad, height=30, width=4, yscrollcommand=bar_editor.set, undo=True)
+        linepad.config(fg='#f8f8f2', bg='#002b36', insertbackground='white', state=GUI.DISABLED)
+        lb = GUI.Listbox(frame_autocomplete, height=4, width=120)
+        lb.config(fg='gray', bg='#002b36')
+        inputpad = GUI.Text(W1, height=30, width=30)
+        inputpad.config(fg='white', bg='#002b36', insertbackground='white')
+
+        W1.add(pad)
+        W1.add(inputpad)
+
+        def share_bar(*event):
+            """
+            the linenumber abd code textpads share same scrollbar
+            :param event: event
+            """
+            pad.yview(*event)
+            linepad.yview(*event)
+
+        bar_editor.config(command=share_bar)
+
+        outputpad = ScrolledText.ScrolledText(frame_outputpad, height=5, width=80)
+        outputpad.config(fg='white', bg='#002b36', insertbackground='white', state=GUI.DISABLED)
+
+        primary_frame.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
+        frame_codepad.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+        linepad.pack(side=GUI.LEFT, fill=GUI.Y)
+        W1.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
+        bar_editor.pack(side=GUI.LEFT, fill=GUI.Y)
+        frame_autocomplete.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+        lb.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+
+        self.book.add(primary_frame, text='untitled.cpp')
+
+        self.primary_frame.append(primary_frame)
+        self.frame_codepad.append(frame_codepad)
+        self.frame_outputpad.append(frame_outputpad)
+        self.frame_autocomplete.append(frame_autocomplete)
+        self.bar_editor.append(bar_editor)
+        self.lb.append(lb)
+        self.pad.append(pad)
+        self.linepad.append(linepad)
+        self.inputpad.append(inputpad)
+        self.outputpad.append(outputpad)
+        self.W1.append(W1)
+
+    def destroy_tab(self):
+        index = self.book.index(self.book.select())
+        self.book.forget(self.book.select())
+        self.primary_frame.pop(index)
+        self.frame_codepad.pop(index)
+        self.frame_outputpad.pop(index)
+        self.frame_autocomplete.pop(index)
+        self.bar_editor.pop(index)
+        self.lb.pop(index)
+        self.pad.pop(index)
+        self.linepad.pop(index)
+        self.inputpad.pop(index)
+        self.outputpad.pop(index)
+        self.W1.pop(index)
 
 
 def main():
-    MyDict = EC.Trie()
-    File = EC.FileDetails()
-    EC.setKeys()
-    Display = EC.codeDisplay()
-    cmd_file = EC.fileFileMenu()
-    edit = EC.editFileMenu()
-    run = EC.runFilemenu()
+    app = GUI.Tk()
+    commands.setKeys()
+    display = commands.CodeDisplay()
+    cmd_file = commands.FilesFileMenu()
+    edit = commands.EditFileMenu()
+    run = commands.RunFilemenu()
+
+    book = ttk.Notebook(app)
+
+    def useless():
+        print 'sexy'
+
+    def index():
+        return book.index(book.select())
 
     def change_lang(newlang):
         """
@@ -51,121 +129,46 @@ def main():
         """
         global lang
         lang = newlang
-        r = map(str, app.title().split('.'))
-        if lang == 'c++' and r[-1] == 'py':
-            app.title('untitled.cpp')
-            cmd_file.set_new_filedetails('untitled.cpp', os.getcwd() + '/untitled.cpp')
-            print File.name, File.path
-        if lang == 'py' and r[-1] == 'cpp':
-            app.title('untitled.py')
-            cmd_file.set_new_filedetails('untitled.py', os.getcwd() + '/untitled.py')
+        # r = map(str, app.title().split('.'))
+        # if lang == 'c++' and r[-1] == 'py':
+        #     app.title('untitled.cpp')
+        #     cmd_file.set_new_filedetails('untitled.cpp', os.getcwd() + '/untitled.cpp')
+        #     print File.name, File.path
+        # if lang == 'py' and r[-1] == 'cpp':
+        #     app.title('untitled.py')
+        #     cmd_file.set_new_filedetails('untitled.py', os.getcwd() + '/untitled.py')
 
     change_lang('c++')
 
-    def hello():
-        c = map(int, pad.index(GUI.INSERT).split('.'))
-        c[-1] -= 1
-        pad.delete(str(c[0]) + '.' + str(c[-1]), GUI.END)
-
-    def shareBar(*args):
-        """
-        the linenumber abd code textpads share same scrollbar
-        :param args: event
-        :return:
-        """
-        pad.yview(*args)
-        linepad.yview(*args)
-
-    def shareMouseWheel(event):
-        """
-        the linenumber abd code textpads share mousewheel
-        :param event:
-        :return:
-        """
-        pad.yview('scroll', event.delta, 'units')
-        linepad.yview('scroll', -1 * (event.delta / 120), 'units')
-        return 'break'
-
-    frameA = GUI.Frame(book)
-    frame1 = GUI.Frame(frameA)
-    frame12 = GUI.Frame(frameA)
-    frame2 = GUI.Frame(frameA)
-    W1 = GUI.PanedWindow(frame1, height=30, width=70)
-    bar_editor = GUI.Scrollbar(frame1)
-
-    # 272822
-
-    pad = GUI.Text(
-        W1,
-        height=30,
-        width=60,
-        yscrollcommand=bar_editor.set,
-        undo=True)
-    pad.config(fg='#f8f8f2', bg='#002b36', insertbackground='white')
-
-    linepad = GUI.Text(
-        frame1,
-        height=30,
-        width=4,
-        yscrollcommand=bar_editor.set,
-        undo=True)
-    linepad.config(
-        fg='#f8f8f2',
-        bg='#002b36',
-        insertbackground='white',
-        state=GUI.DISABLED)
-
-    lb = GUI.Listbox(frame12, height=4, width=120)
-    lb.config(fg='gray',bg='#002b36')
-    # lb.pack(fill=GUI.X)
-
-    inputpad = GUI.Text(
-        W1,
-        height=30,
-        width=30)
-    inputpad.config(fg='white', bg='#002b36', insertbackground='white')
-
-    W1.add(pad)
-    W1.add(inputpad)
-
-    bar_editor.config(command=shareBar)
-    # 2aa198
-    outputpad = ScrolledText.ScrolledText(frame2, height=5, width=80)
-    outputpad.config(
-        fg='white',
-        bg='#002b36',
-        insertbackground='white',
-        state=GUI.DISABLED)
+    code = DisplayWidget(book)
+    code.create_tab()
 
     menubar = GUI.Menu(app)
     filemenu = GUI.Menu(menubar, tearoff=0)
-    filemenu.add_command(label='New', command=hello)
-    filemenu.add_command(
-        label='Open',
-        command=lambda: cmd_file.Open(
-            app,
-            pad,
-            linepad,
-            lang))
-    filemenu.add_command(label='Save', command=lambda: cmd_file.Save(app, pad))
-    filemenu.add_command(
-        label='Save As',
-        command=lambda: cmd_file.Save_As(
-            app,
-            pad))
+    filemenu.add_command(label='New', command=useless)
+    filemenu.add_command(label='New Tab', command=lambda: cmd_file.create_new_tab(code))
+    filemenu.add_command(label='Close Tab', command=lambda: cmd_file.close_tab(code))
+    filemenu.add_command(label='Open', command=lambda: cmd_file.Open(
+        app, book, code.pad[index()], code.linepad[index()], lang))
+    filemenu.add_command(label='Save', command=lambda: cmd_file.Save(
+        app, code.pad[index()]))
+    filemenu.add_command(label='Save As', command=lambda: cmd_file.Save_As(
+        app, code.pad[index()]))
     filemenu.add_command(label='Exit', command=cmd_file.Exit)
     menubar.add_cascade(label='File', menu=filemenu)
 
     editmenu = GUI.Menu(menubar, tearoff=0)
-    editmenu.add_command(label='Undo - (ctrl+z)', command=lambda: edit.undo(pad, linepad))
-    editmenu.add_command(label='Redo - (ctrl+r)', command=lambda: edit.redo(pad, linepad))
+    editmenu.add_command(label='Undo - (ctrl+z)', command=lambda: edit.undo(
+        code.pad[index()], code.linepad[index()]))
+    editmenu.add_command(label='Redo - (ctrl+r)', command=lambda: edit.redo(
+        code.pad[index()], code.linepad[index()]))
     menubar.add_cascade(label='Edit', menu=editmenu)
 
     runmenu = GUI.Menu(menubar, tearoff=0)
     runmenu.add_command(label='Compile - (F7)', command=lambda: run.compile(
-        app, pad, outputpad, lang, frame2))
+        app, code.pad[index()], code.outputpad[index()], lang, code.frame_outputpad[index()]))
     runmenu.add_command(label='Run - (F5)', command=lambda: run.run(
-        app, pad, outputpad, inputpad, lang, frame2))
+        app, code.pad[index()], code.outputpad[index()], code.inputpad[index()], lang, code.frame_outputpad[index()]))
     menubar.add_cascade(label='Run', menu=runmenu)
 
     langmenu = GUI.Menu(menubar, tearoff=0)
@@ -173,41 +176,68 @@ def main():
     langmenu.add_radiobutton(label='python', command=lambda: change_lang('py'))
     menubar.add_cascade(label='Language', menu=langmenu)
 
-    pad.bind('<Tab>', lambda event: Display.tab_width(pad, event))
-    app.bind('<Escape>', lambda event: Display.escape(frame12,pad, event))
-    app.bind('<Down>', lambda event: Display.select_first(frame12,lb ,pad,event))
-    frame12.bind('<Tab>', lambda event: Display.insert_word(frame12, pad, lb, lang, event))
+    app.bind('<Escape>', lambda event: display.escape(
+        code.frame_autocomplete[index()], code.pad[index()], event))
+    app.bind('<Down>', lambda event: display.select_first(
+        code.frame_autocomplete[index()], code.lb[index()], code.pad[index()], event))
 
-    app.bind('<KeyPress>', lambda event: Display.show_in_console(
-        event, pad, linepad, lang, lb,frame1,W1,bar_editor,frame2,frame12,outputpad))
+    code.frame_autocomplete[index()].bind('<Return>', lambda event: display.insert_word(
+        code.frame_autocomplete[index()], code.pad[index()], code.lb[index()], lang, event))
 
-    app.bind('<space>', lambda event: Display.add_to_trie(frame12, pad, lb, event))
-    app.bind('<Return>', lambda event: Display.indentation(pad, linepad, lang, event))
-    app.bind('<F7>', lambda event: run.compile(app, pad, outputpad, lang, frame2, event))
+    code.pad[index()].bind('<Tab>', lambda event: display.tab_width(code.pad[index()], event))
+
+    app.bind('<KeyPress>', lambda event: display.show_in_console(
+        app, book, event, code.pad[index()], code.linepad[index()], lang, code.lb[index()],
+        code.frame_codepad[index()], code.W1[index()], code.bar_editor[index()],
+        code.frame_outputpad[index()], code.frame_autocomplete[index()], code.outputpad[index()]))
+
+    app.bind('<space>', lambda event: display.add_to_trie(
+        code.frame_autocomplete[index()], code.pad[index()], code.lb[index()], event))
+    app.bind('<Return>', lambda event: display.indentation(
+        code.pad[index()], code.linepad[index()], lang, event))
+    app.bind('<F7>', lambda event: run.compile(
+        app, code.pad[index()], code.outputpad[index()], lang, code.frame_outputpad[index()], event))
     app.bind('<F5>', lambda event: run.run(
-        app, pad, outputpad, inputpad, lang,frame2, event))
+        app, code.pad[index()], code.outputpad[index()], code.inputpad[index()],
+        lang, code.frame_outputpad[index()], event))
 
-    app.bind('<Control-r>', lambda event: edit.redo(pad, linepad, lang, event))
-    app.bind('<Control-z>', lambda event: edit.undo(pad, linepad, lang, event))
-    app.bind('<BackSpace>', lambda event: Display.fast_backspace(pad, linepad, event))
+    app.bind('<Control-r>', lambda event: edit.redo(
+        code.pad[index()], code.linepad[index()], lang, event))
+    app.bind('<Control-z>', lambda event: edit.undo(
+        code.pad[index()], code.linepad[index()], lang, event))
+    app.bind('<BackSpace>', lambda event: display.fast_backspace(
+        code.pad[index()], code.linepad[index()], event))
 
-    book.add(frameA,text='Tab 1')
+    # TODO : Move these somwehre else
 
-    book.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
-    frameA.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
+    def add_tab(*args):
+        cmd_file.create_new_tab(code)
+        r = app.geometry().split('+')
+        coordinates = map(int,r[0].split('x'))
+        new_tab_button.place_forget()
+        new_tab_button.place(x = coordinates[0] - 55, y = -0.5)
 
-    frame1.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
-    linepad.pack(side=GUI.LEFT, fill=GUI.Y)
-    W1.pack(side=GUI.LEFT, fill=GUI.BOTH, expand=GUI.YES)
-    bar_editor.pack(side=GUI.LEFT, fill=GUI.Y)
-    frame12.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
-    lb.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
-    # frame2.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
-    # outputpad.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
+    def remove_tab(*args):
+        cmd_file.close_tab(code)
+        r = app.geometry().split('+')
+        coordinates = map(int,r[0].split('x'))
+        del_tab_button.place_forget()
+        del_tab_button.place(x = coordinates[0] - 25, y = -0.5)
+
+    new_tab_button = GUI.Button(book,text = '++',bg='#228b22',fg='white')
+    new_tab_button.place(x = 745, y = -0.5)
+    new_tab_button.bind('<Button-1>',add_tab)
+
+    del_tab_button = GUI.Button(book,text = '--',bg='maroon',fg='white')
+    del_tab_button.place(x = 775, y = -0.5)
+    del_tab_button.bind('<Button-1>',remove_tab)
+
+    book.pack(side=GUI.TOP, fill=GUI.BOTH, expand=GUI.YES)
     app.config(menu=menubar)
-
+    app.geometry('800x600')
+    app.title('untitled.cpp')
+    app.mainloop()
 
 if __name__ == '__main__':
     main()
-    app.title('untitled.cpp')
-    app.mainloop()
+
